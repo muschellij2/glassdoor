15a8844a3609dd3969c63abc3831df7af40e4574
muschellij2 <muschellij2@gmail.com>
2017-10-12 15:10:44 -0400

    Updated to rbuildignore.

--- sticker.R
+++ sticker.R
@@ -7,11 +7,14 @@ outline = "black"
 # background = "#0caa41"
 background = "dodgerblue2"
 p_color = "black"
-sticker("icon.png",	
+sticker("icon.png",
         package = package,
         h_fill = background,
-        h_color = outline, 
-        s_width = 0.2, 
+        h_color = outline,
+        s_width = 0.2,
         s_height = 0.4,
         s_x = 1,
         filename = "sticker.png")
+
+usethis::use_build_ignore(
+  c("icon.png", "sticker.R", "sticker.png"))
